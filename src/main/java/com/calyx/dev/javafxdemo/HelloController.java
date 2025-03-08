1fc836c73b60d89bd3a9583d594e1dd572a7c326
florenz-ramos <florenzramos20@gmail.com>
2025-03-08 08:43:14 +0800

    Changes from richard_dev

--- src/main/java/com/calyx/dev/javafxdemo/HelloController.java
+++ src/main/java/com/calyx/dev/javafxdemo/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Hello World!");
+        welcomeText.setText("Richard Dev");
     }
 }
